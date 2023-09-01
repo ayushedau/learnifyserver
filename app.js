0bfa163725ff3621fac4edb5bb46f389e6751ef2
Ayush Hedau <76205469+ayushedau@users.noreply.github.com>
2023-09-01 23:13:52 +0530

    Modifying app.js

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ app.use(
     })
 );
 app.use(cookieParser());
-app.use("*",cors({
+app.use(cors({
     origin:process.env.FRONTEND_URL,
-    // origin:true,
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }));
